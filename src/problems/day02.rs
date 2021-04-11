5ed2edc9c9d0500d59a446bab4cc4f0dfdf2cf29
Kliment Mamykin <kmamyk@gmail.com>
2021-04-11 10:37:07 -0400

    Day 2 problem 2. Needs cleanup

--- src/problems/day02.rs
+++ src/problems/day02.rs
@@ -6,33 +6,34 @@ use std::num::ParseIntError;
 use std::path::Path;
 use std::str::FromStr;
 
-#[derive(Debug, PartialEq)]
-struct Password {
-    password: String,
-}
-
-impl Password {
-    fn count_occurrences(&self, ch: char) -> i32 {
-        self.password
-            .chars()
-            .filter(|&c| c == ch)
-            .count()
-            .try_into()
-            .unwrap()
-    }
-}
+// #[derive(Debug, PartialEq)]
+// struct Password {
+//     password: String,
+// }
+//
+// impl Password {
+//     fn count_occurrences(&self, ch: char) -> usize {
+//         self.password.chars().filter(|&c| c == ch).count()
+//     }
+// }
 
 #[derive(Debug, PartialEq)]
 struct PasswordPolicy {
-    min_occurrences: i32,
-    max_occurrences: i32,
+    min_occurrences: usize,
+    max_occurrences: usize,
     character: char,
 }
 
 impl PasswordPolicy {
-    fn satisfies(&self, password: &Password) -> bool {
-        let actual_char_count = password.count_occurrences(self.character);
-        actual_char_count >= self.min_occurrences && actual_char_count <= self.max_occurrences
+    // fn satisfies(&self, password: &Password) -> bool {
+    //     let actual_char_count = password.count_occurrences(self.character);
+    //     actual_char_count >= self.min_occurrences && actual_char_count <= self.max_occurrences
+    // }
+
+    fn is_satisfied_by(&self, password: &str) -> bool {
+        let first = password.chars().nth(self.min_occurrences - 1) == Some(self.character);
+        let second = password.chars().nth(self.max_occurrences - 1) == Some(self.character);
+        return (first & !second) | (!first & second);
     }
 }
 
@@ -43,8 +44,8 @@ impl FromStr for PasswordPolicy {
 
         let minmax: Vec<&str> = parts[0].split('-').collect();
 
-        let min = minmax[0].parse::<i32>()?;
-        let max = minmax[1].parse::<i32>()?;
+        let min = minmax[0].parse::<usize>()?;
+        let max = minmax[1].parse::<usize>()?;
         let char = parts[1].chars().nth(0).unwrap_or(' ');
         Ok(PasswordPolicy {
             min_occurrences: min,
@@ -54,19 +55,17 @@ impl FromStr for PasswordPolicy {
     }
 }
 
-fn parse_password_policy_pair(s: &str) -> Option<(PasswordPolicy, Password)> {
+fn parse_password_policy_pair(s: &str) -> Option<(PasswordPolicy, String)> {
     let parts: Vec<&str> = s.split(':').collect();
     let policy = parts[0].parse::<PasswordPolicy>().unwrap();
-    let password = Password {
-        password: parts[1].to_string(),
-    };
+    let password = parts[1].trim().to_string();
     return Some((policy, password));
 }
 
-fn read_password_policy_pairs_from_file(s: &str) -> Result<Vec<(PasswordPolicy, Password)>, Error> {
+fn read_password_policy_pairs_from_file(s: &str) -> Result<Vec<(PasswordPolicy, String)>, Error> {
     let path = Path::new(s);
     let input = File::open(path)?;
-    let list: Vec<(PasswordPolicy, Password)> = BufReader::new(input)
+    let list: Vec<(PasswordPolicy, String)> = BufReader::new(input)
         .lines()
         .map(|l| l.unwrap())
         .map(|l| parse_password_policy_pair(&l).unwrap())
@@ -79,10 +78,15 @@ pub fn day02() {
     assert_eq!(10, p.min_occurrences);
     assert_eq!(19, p.max_occurrences);
     println!("{:?}", p);
+    assert!("5-8 h"
+        .parse::<PasswordPolicy>()
+        .unwrap()
+        .is_satisfied_by("tfhhtsjhhhsddl"));
+
     let list =
         read_password_policy_pairs_from_file("./inputs/day02_1").expect("Could not read file");
-    let n_valid_passwords = list.iter().filter(|(pp, p)| pp.satisfies(&p)).count();
-    println!("{:?}", n_valid_passwords);
-    // list.iter()
-    //     .for_each(|(pp, p)| println!("{:?} : {:?}", pp, p))
+    // let n_satisfies = list.iter().filter(|(pp, p)| pp.satisfies(&p)).count();
+    // println!("{:?}", n_satisfies);
+    let n_satisfies2 = list.iter().filter(|(pp, p)| pp.is_satisfied_by(&p)).count();
+    println!("{:?}", n_satisfies2);
 }
