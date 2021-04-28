d2626d6e939e89987b3113dd6cd21ef0c597e8f1
Kliment Mamykin <kmamyk@gmail.com>
2021-04-27 22:58:56 -0400

    Day 8 problenm 1

--- src/problems/day08.rs
+++ src/problems/day08.rs
@@ -1,10 +1,6 @@
 use super::super::utils::read_strings_from_file;
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::str::FromStr;
-use std::num::ParseIntError;
 use itertools::Itertools;
-use std::path::StripPrefixError;
+use std::str::FromStr;
 
 pub fn solve() {
     let strings = read_strings_from_file("./inputs/day08_1").expect("Failed to read inputs");
@@ -12,13 +8,16 @@ pub fn solve() {
     // println!("Problem 2: {:?}", problem_2(&strings));
 }
 
-pub fn problem_1(strings: &Vec<String>) -> usize {
+pub fn problem_1(strings: &Vec<String>) -> i32 {
     let instructions: Vec<Instruction> = strings
         .iter()
         .map(|s| s.parse::<Instruction>().unwrap())
         .collect();
     println!("{:?}", instructions);
-    5
+    let mut executor = CodeExecutor::new(instructions);
+    executor.execute();
+    println!("{:?}", executor.state);
+    executor.state.accumulator
 }
 
 pub fn problem_2(strings: &Vec<String>) -> usize {
@@ -39,17 +38,82 @@ impl FromStr for Instruction {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Option<(&str, &str)> = s
-            .split(' ')
-            .map(|s| s.trim())
-            .collect_tuple();
+        let parts: Option<(&str, &str)> = s.split(' ').map(|s| s.trim()).collect_tuple();
         if let Some((a, b)) = parts {
             let operation = a.trim().to_string();
             let argument = b.parse::<i32>().unwrap();
-            Ok(Instruction{ operation, argument })
+            Ok(Instruction {
+                operation,
+                argument,
+            })
         } else {
             Err("Can not parse string".to_string())
         }
+    }
+}
+
+impl Instruction {
+    fn execute(&self, state: &ExecutionState) -> ExecutionState {
+        match &self.operation[..] {
+            "acc" => ExecutionState {
+                accumulator: state.accumulator + self.argument,
+                address: state.address + 1,
+            },
+            "jmp" => ExecutionState {
+                accumulator: state.accumulator,
+                address: state.address + self.argument,
+            },
+            "nop" => ExecutionState {
+                accumulator: state.accumulator,
+                address: state.address + 1,
+            },
+            _ => ExecutionState {
+                accumulator: state.accumulator,
+                address: state.address,
+            },
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ExecutionState {
+    accumulator: i32,
+    address: i32,
+}
+impl ExecutionState {}
+
+#[derive(Debug, PartialEq)]
+struct CodeExecutor {
+    instructions: Vec<Instruction>,
+    loop_tracker: Vec<bool>,
+    state: ExecutionState,
+}
+
+impl CodeExecutor {
+    fn new(instructions: Vec<Instruction>) -> CodeExecutor {
+        let loop_tracker = vec![false; instructions.len()];
+        let state = ExecutionState {
+            accumulator: 0,
+            address: 0,
+        };
+        Self {
+            instructions,
+            loop_tracker,
+            state,
+        }
+    }
+
+    fn execute(&mut self) -> () {
+        loop {
+            let new_state = self.instructions[self.state.address as usize].execute(&self.state);
+            if self.loop_tracker[new_state.address as usize] {
+                break;
+            } else {
+                self.loop_tracker[new_state.address as usize] = true;
+                self.state = new_state;
+            }
+        }
+        ()
     }
 }
 
@@ -60,18 +124,12 @@ mod tests {
     #[test]
     fn test1() {
         let strings: Vec<String> = vec![
-            "nop +0",
-            "acc +1",
-            "jmp +4",
-            "acc +3",
-            "jmp -3",
-            "acc -99",
-            "acc +1",
-            "jmp -4",
+            "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4",
             "acc +6",
-        ].iter()
-            .map(|s| s.to_string())
-            .collect();
+        ]
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
         assert_eq!(5, problem_1(&strings));
     }
 }
