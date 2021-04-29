22a3ea967eb3141ef59998a5a614dddc85a70f99
Kliment Mamykin <kmamyk@gmail.com>
2021-04-28 21:05:07 -0400

    Day 8 problem 2

--- src/problems/day08.rs
+++ src/problems/day08.rs
@@ -5,7 +5,7 @@ use std::str::FromStr;
 pub fn solve() {
     let strings = read_strings_from_file("./inputs/day08_1").expect("Failed to read inputs");
     println!("Problem 1: {:?}", problem_1(&strings));
-    // println!("Problem 2: {:?}", problem_2(&strings));
+    println!("Problem 2: {:?}", problem_2(&strings));
 }
 
 pub fn problem_1(strings: &Vec<String>) -> i32 {
@@ -14,21 +14,36 @@ pub fn problem_1(strings: &Vec<String>) -> i32 {
         .map(|s| s.parse::<Instruction>().unwrap())
         .collect();
     println!("{:?}", instructions);
-    let mut executor = CodeExecutor::new(instructions);
-    executor.execute();
-    println!("{:?}", executor.state);
-    executor.state.accumulator
+    let executor = CodeExecutor::new();
+    let result = executor.execute(&instructions);
+    println!("{:?}", result);
+    match result {
+        ExecutionResult::InfiniteLoop(state) => state.accumulator,
+        ExecutionResult::Success(state) => state.accumulator,
+    }
 }
 
-pub fn problem_2(strings: &Vec<String>) -> usize {
-    let rules: Vec<Instruction> = strings
+pub fn problem_2(strings: &Vec<String>) -> Option<i32> {
+    let instructions: Vec<Instruction> = strings
         .iter()
         .map(|s| s.parse::<Instruction>().unwrap())
         .collect();
-    1
+    println!("{:?}", instructions);
+    let code_generator = CodeCandidateGenerator{ instructions, index: 0 };
+    let executor = CodeExecutor::new();
+    for code_candidate in code_generator {
+        println!("Candidate: {:?}", code_candidate);
+        let result = executor.execute(&code_candidate);
+        println!("{:?}", result);
+        match result {
+            ExecutionResult::Success(state) => { return Some(state.accumulator); },
+            ExecutionResult::InfiniteLoop(state) => { continue; },
+        }
+    }
+    None
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, Clone, PartialEq)]
 struct Instruction {
     operation: String,
     argument: i32,
@@ -80,43 +95,72 @@ struct ExecutionState {
     accumulator: i32,
     address: i32,
 }
+
 impl ExecutionState {}
 
 #[derive(Debug, PartialEq)]
-struct CodeExecutor {
-    instructions: Vec<Instruction>,
-    loop_tracker: Vec<bool>,
-    state: ExecutionState,
+enum ExecutionResult {
+    Success(ExecutionState),
+    InfiniteLoop(ExecutionState),
 }
 
+#[derive(Debug, PartialEq)]
+struct CodeExecutor {}
+
 impl CodeExecutor {
-    fn new(instructions: Vec<Instruction>) -> CodeExecutor {
-        let loop_tracker = vec![false; instructions.len()];
-        let state = ExecutionState {
+    fn new() -> CodeExecutor {
+        Self {}
+    }
+
+    fn execute(&self, instructions: &Vec<Instruction>) -> ExecutionResult {
+        let mut state = ExecutionState {
             accumulator: 0,
             address: 0,
         };
-        Self {
-            instructions,
-            loop_tracker,
-            state,
-        }
-    }
-
-    fn execute(&mut self) -> () {
+        let mut loop_tracker = vec![false; instructions.len()];
         loop {
-            let new_state = self.instructions[self.state.address as usize].execute(&self.state);
-            if self.loop_tracker[new_state.address as usize] {
-                break;
-            } else {
-                self.loop_tracker[new_state.address as usize] = true;
-                self.state = new_state;
+            let new_state = instructions[state.address as usize].execute(&state);
+            if new_state.address as usize >= instructions.len() {
+                return ExecutionResult::Success(new_state);
+            } else if loop_tracker[new_state.address as usize] {
+                return ExecutionResult::InfiniteLoop(state);
+            } else  {
+                loop_tracker[new_state.address as usize] = true;
+                state = new_state;
             }
         }
-        ()
     }
 }
 
+struct CodeCandidateGenerator {
+    instructions: Vec<Instruction>,
+    index: usize,
+}
+
+impl Iterator for CodeCandidateGenerator {
+    type Item = Vec<Instruction>;
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(instruction) = self.instructions.get(self.index) {
+            match &instruction.operation[..] {
+                "acc" => {
+                    self.index += 1;
+                },
+                _ => {
+                    // jmp or nop instruction
+                    let mut mutated_code: Vec<Instruction> = self.instructions.iter().cloned().collect();
+                    mutated_code[self.index] = if instruction.operation == "jmp" {
+                        Instruction{operation: "nop".to_string(), argument: instruction.argument }
+                    } else {
+                        Instruction{operation: "jmp".to_string(), argument: instruction.argument }
+                    };
+                    self.index += 1;
+                    return Some(mutated_code);
+                }
+            }
+        }
+        None
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -127,9 +171,28 @@ mod tests {
             "nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4",
             "acc +6",
         ]
-        .iter()
-        .map(|s| s.to_string())
-        .collect();
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
         assert_eq!(5, problem_1(&strings));
+    }
+
+    #[test]
+    fn test2() {
+        let strings: Vec<String> = vec![
+            "nop +0",
+            "acc +1",
+            "jmp +4",
+            "acc +3",
+            "jmp -3",
+            "acc -99",
+            "acc +1",
+            "jmp -4",
+            "acc +6",
+        ]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+        assert_eq!(Some(8), problem_2(&strings));
     }
 }
